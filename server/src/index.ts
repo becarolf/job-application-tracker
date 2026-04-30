4024281d98b59a6c990c904df6653bc3d77853a9
becarolf <becarolf@gmail.com>
2026-04-29 20:53:58 -0400

    feat: add endpoint (POST route) to create job applications

--- server/src/index.ts
+++ server/src/index.ts
@@ -54,6 +54,31 @@ app.get("/api/applications", (req, res) => {
   res.json(jobApplications);
 });
 
+
+app.post("/api/applications", (req, res) => {
+  const { company, jobTitle, jobUrl, location, status, dateApplied } = req.body;
+
+  if (!company || !jobTitle) {
+    return res.status(400).json({
+      error: "Company and job title are required.",
+    });
+  }
+
+  const newApplication: JobApplication = {
+    id: jobApplications.length + 1,
+    company,
+    jobTitle,
+    jobUrl: jobUrl || "",
+    location: location || "",
+    status: status || "Saved",
+    dateApplied: dateApplied || new Date().toISOString().split("T")[0],
+  };
+
+  jobApplications.push(newApplication);
+
+  res.status(201).json(newApplication); // 201 means "created"
+});
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
